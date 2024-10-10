7e7254ac9cf7a13fc129fb05bd39b265c609deae
hben35096 <139383150+hben35096@users.noreply.github.com>
2024-10-10 13:26:44 +0800

    Update Joy_caption_node.py

--- Joy_caption_node.py
+++ Joy_caption_node.py
@@ -178,7 +178,7 @@ class Joy_caption:
         # Tokenize the prompt
         prompt = tokenizer.encode(prompt, return_tensors='pt', padding=False, truncation=False, add_special_tokens=False)
         # Embed image
-        with torch.amp.autocast_mode.autocast(str(DEVICE), enabled=True):
+        with torch.amp.autocast_mode.autocast(device_type=DEVICE.type, enabled=True):
             vision_outputs = clip_model(pixel_values=pImge, output_hidden_states=True)
             image_features = vision_outputs.hidden_states[-2]
             embedded_images = image_adapter(image_features)
